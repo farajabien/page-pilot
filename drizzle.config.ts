fe567dd11adacb4389990b6a55bf354c7944f04c
Bienvenu <faraja.bien@gmail.com>
2024-10-15 07:43:27 +0300

    UPDATE NOVEL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
@@ -5,7 +8,8 @@ export default defineConfig({
   out: "./drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,     
+    url:
+      process.env.POSTGRES_URL ||
+      "postgres://default:w5NbIsLQXpW0@ep-silent-base-a4zctjfb-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require",
   },
 });
-
